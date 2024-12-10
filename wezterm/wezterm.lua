a99d8ff2868f4190bc2fa6f31c7c3640d738e436
bansonsy <bansonsy@gmail.com>
2024-12-10 14:54:28 -0800

    Wezterm Updated Config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -31,7 +31,7 @@ config = {
           },
           width = "100%",
           height = "100%",
-          opacity = 0.55,
+          opacity = 0.85,
         },
       },
       window_padding = {
